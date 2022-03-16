331984f1ec9ca2ecd81e8828cbdf7ba004111c02
Loic Brugneaux <loic.brugneaux1@gmail.com>
2022-03-16 16:45:15 +0100

    Inserting bird behaviour + platform behaviour

---
+++ src/Assets/Makememove.cs
@@ -0,0 +1,39 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Makememove : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 velocity;
+    private bool moving;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+            moving = false;
+            
+            Debug.Log("pouet");
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            transform.position += (velocity * Time.deltaTime);
+
+        }
+    }
+}
